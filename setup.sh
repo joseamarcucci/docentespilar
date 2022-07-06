167670c63436a59a5b2bf08c993fbf59702f9823
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2022-07-06 16:58:12 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,3 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
